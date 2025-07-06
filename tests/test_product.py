d8b0044afeeaf827126b38f27f0cb0656e5040e8
ellGoltso <goltsovi3@gmail.com>
2025-07-06 15:45:48 +0300

    create classes and tests

---
+++ tests/test_product.py
@@ -0,0 +1,5 @@
+def test_product_init(product_xiaomi):
+    assert product_xiaomi.name == "Xiaomi Redmi Note 11"
+    assert product_xiaomi.description == "1024GB, Синий"
+    assert product_xiaomi.price == 31000.0
+    assert product_xiaomi.quantity == 14
